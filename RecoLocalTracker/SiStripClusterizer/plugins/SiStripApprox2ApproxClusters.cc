6b430de4c22e71d1029022941a785e4800e8817a
Klitos <kleitos.savva.97@gmail.com>
2023-04-26 17:12:55 +0200

    Working towards fixing the peak filter

--- RecoLocalTracker/SiStripClusterizer/plugins/SiStripApprox2ApproxClusters.cc
+++ RecoLocalTracker/SiStripClusterizer/plugins/SiStripApprox2ApproxClusters.cc
@@ -61,6 +61,8 @@ void SiStripApprox2ApproxClusters::produce(edm::Event& event, edm::EventSetup co
       uint8_t width = cluster.width();
       float avgCharge = cluster.avgCharge();
       bool isSaturated = cluster.isSaturated();
+      bool trimFilter = cluster.trimFilter();
+      bool peakFilter = cluster.peakFilter();
 
       switch (approxVersion) {
         case 0:  //ORIGINAL
@@ -87,7 +89,7 @@ void SiStripApprox2ApproxClusters::produce(edm::Event& event, edm::EventSetup co
           break;
       }
 
-      ff.push_back(SiStripApproximateCluster(barycenter, width, avgCharge, isSaturated));
+      ff.push_back(SiStripApproximateCluster(barycenter, width, avgCharge, isSaturated, trimFilter, peakFilter));
     }
   }
 
